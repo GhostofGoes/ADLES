579b287fbfb929d17de2238f428c422ca4554a54
Christopher Goes <ghostofgoes@gmail.com>
2016-10-27 00:06:33 -0700

    Better parser structure

--- parser/parser.py
+++ parser/parser.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import yaml
 
-
-def main():
-    """ For testing of the parser """
-
-    # Reference: http://pyyaml.org/wiki/PyYAMLDocumentation
-    filename = 'test_example.yaml'
+# Reference: http://pyyaml.org/wiki/PyYAMLDocumentation
+def parse_file(filename):
+    # TODO: doctest
+    import yaml
     with open(filename, 'r') as f:
         try:
             doc = yaml.safe_load(f)  # Parses the YAML file and creates a python object with it's structure and contents
@@ -19,7 +16,15 @@ def main():
                 print("Error position: (%s:%s)" % (mark.line + 1, mark.column + 1))
             else:
                 print("Error: ", exc)
-            exit()  # If there was an error, then there ain't gonna be any markup, so we exit
+            return None  # If there was an error, then there ain't gonna be any markup, so we exit in a obvious way
+    return doc
+
+
+def main():
+    """ For testing of the parser """
+
+    filename = 'test_example.yaml'
+    doc = parse_file(filename)
 
     print(doc["name"])
 
