4c91655475fb991c2ebd87966ca95197acaf9a88
Christopher Goes <ghostofgoes@gmail.com>
2016-11-27 12:39:14 -0700

    Cleaned up main
    Still a lot to do for the basics

--- main.py
+++ main.py
@@ -6,24 +6,21 @@
 """main
 
 Usage:
-    main.py --logins FILE [options]
-    main.py (-i | --interactive) [options]
-    main.py [options]
+    main.py --environment FILE --logins FILE --exercise FILE [Options]
+    main.py --interactive
+    main.py --package-dir
+    main.py [Options]
     main.py --version
     main.py (-h | --help)
 
 Options:
-    -h, --help      Duh
-    --version       Double duh
-    --logins        File with login information for vSphere
-    --interactive   Interactive input of login information
-    -s, --host      vCenter server to connect to
-    -o, --port      Port to connect on
-    -u, --user      Username to login with
-    -p, --password  Password to login with
-    -f, --file      File with one or more actions to perform
-    -a, --action    Action to perform
-    -m, --method    Method used to perform action
+    -h, --help          Shows this help
+    --version           Prints current version
+    --interactive       Interactive input of needed information
+    --environment FILE  YAML file with environment confirmation information [default: environment.yaml]
+    --logins FILE       JSON file with login information for the environment [default: logins.json]
+    --exercise FILE     YAML file with the exercise specification [default: exercise.yaml]
+    --package-dir       Name of the exercise package directory
 
 """
 
@@ -34,36 +31,33 @@ from pyVim.connect import SmartConnect, Disconnect
 import logging
 
 from automation import vsphere
+from parser.parser import parse_file
 
 __version__ = "0.1.0"
-__author__ = "Christopher Goes <cgoes@uidaho.edu>"
+__author__  = "Christopher Goes"
+__email__   = "<cgoes@uidaho.edu>"
 
 logger = logging.getLogger(__name__)
 
 
 # NOTE: currently this is built around vSphere, using vCenter server and ESXi 6.0 U2
 def main():
-    args = docopt(__doc__, version=__version__, help=True)
 
-    # TODO: could we put server info in the specification file? or have the login filename in the spec file?
     if args["--interactive"]:
         host = input("Hostname of vCenter server: ")
         port = input("Port of vCenter server: ")
         user = input("Username to login with: ")
         pswd = getpass("Password to login with: ")
         server = SmartConnect(host, port, user, pswd)
-    elif args["--logins"]:
+    else:
         from json import dump
         # TODO: login file definition
-    else:
         server = SmartConnect(args["--host"], args["--port"], args["--user"], args["--password"])
-    atexit.register(Disconnect, server)
 
-    if args["-a"]:
-        if args["-a"] == "an_action":
-            pass
+    atexit.register(Disconnect, server)
 
 
 if __name__ == '__main__':
     logging.basicConfig(filename='main.log', level=logging.DEBUG)
+    args = docopt(__doc__, version=__version__, help=True)
     main()
