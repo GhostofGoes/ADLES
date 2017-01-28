7ea95f3b940fd09628e51d6f789e0f739ef37f06
Christopher Goes <ghostofgoes@gmail.com>
2017-01-27 17:31:41 -0800

    vSphere changes
    - Created a class for vSphere to maintain connection, logging, and constants for the instance
    - Added create_folder implementation
    
    So far this is untested. Will write some tests once I have access to testbed

--- automation/vsphere.py
+++ automation/vsphere.py
@@ -1,72 +1,100 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from logging import getLogger
+import logging
+from atexit import register
 
-logger = getLogger(__name__)
-
-
-def clone_vm(vm, clone_name):
-    pass
+from pyVim.connect import SmartConnect, Disconnect
+from pyVmomi import vim
 
 
-def create_vm_from_template():
-    pass
+class vSphere:
+    def __init__(self, user, password, host, port=443):
+        self._log = logging.getLogger(__name__)
+        if not password:
+            from getpass import getpass
+            password = getpass(prompt='Enter password for host %s and user %s: ' % (host, user))
+        self.server = SmartConnect(host=host, user=user, pwd=password, port=int(port))
+        if not self.server:
+            print("Could not connect to the specified host using specified username and password")
+            raise Exception()
+        register(Disconnect, self.server)
+
+    # From: create_folder_in_datacenter.py in pyvmomi-community-samples
+    def create_folder(self, folder, datacenter):
+        """ Creates a VM folder in a datacenter """
+        content = self.server.RetrieveContent()
+        dc = get_obj(content, [vim.Datacenter], datacenter)
+        if get_obj(content, [vim.Folder], folder):
+            self._log.warning("Folder '%s' already exists" % folder)
+        else:
+            dc.vmFolder.CreateFolder(folder)
+
+    def clone_vm(self, vm, clone_name):
+        pass
+
+    def create_vm_from_template(self):
+        pass
+
+    def destroy_vm(self, vm):
+        pass
+
+    def move_vm(self, vm, dest_folder):
+        pass
+
+    def convert_vm_to_template(self, vm, template_name):
+        pass
+
+    def change_vm_power_state(self, vm, power_state):
+        """ Power on, Power off, ACPI shutdown, Reset """
+        pass
+
+    def add_device_to_vm(self, vm, device_spec):
+        pass
+
+    def remove_device_from_vm(self, vm, device):
+        pass
+
+    def modify_device(self, vm, device_name):
+        pass
+
+    def create_network(self, name, type):
+        pass
+
+    def destroy_network(self, name, type):
+        pass
+
+    def modify_network(self, name, type, modification):
+        pass
 
 
-def destroy_vm(vm):
-    pass
-
-
-def move_vm(vm, dest_folder):
-    pass
-
-
-def convert_vm_to_template(vm, template_name):
-    pass
-
-
-def change_vm_power_state(vm, power_state):
-    """ Power on, Power off, ACPI shutdown, Reset """
-    pass
-
-
-def add_device_to_vm(vm, device_spec):
-    pass
-
-
-def remove_device_from_vm(vm, device):
-    pass
-
-
-def modify_device(vm, device_name):
-    pass
-
-
-def create_network(name, type):
-    pass
-
-
-def destroy_network(name, type):
-    pass
-
-
-def modify_network(name, type, modification):
-    pass
+# Helper functions
+def get_obj(content, vimtype, name):
+    obj = None
+    container = content.viewManager.CreateContainerView(
+        content.rootFolder, vimtype, True)
+    for c in container.view:
+        if c.name == name:
+            obj = c
+            break
+    return obj
 
 
 def main():
     """ For testing of vSphere """
     from json import dump
     from os import pardir, path
-    from pyVim.connect import SmartConnect, Disconnect
-    from atexit import register
+    # from pyVim.connect import SmartConnect, Disconnect
+    # from atexit import register
+
+    logging.basicConfig(filename='vsphere_testing.log', filemode='w', level=logging.DEBUG)
 
     with open(path.join(pardir, "logins.json"), "r") as login_file:
         logins = dump(fp=login_file)["vsphere"]
 
-    server = SmartConnect(logins["host"], logins["port"], logins["user"], logins["pass"])
-    register(Disconnect, server)
+    server = vSphere(logins["user"], logins["pass"], logins["host"], logins["port"])
+    # server = SmartConnect(logins["host"], logins["port"], logins["user"], logins["pass"])
+    # register(Disconnect, server)
 
 
 if __name__ == '__main__':
