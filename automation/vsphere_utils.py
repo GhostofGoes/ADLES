5e8b97c1333a2530274740a45b5e50c868874ef5
Christopher Goes <ghostofgoes@gmail.com>
2017-01-31 15:44:13 -0800

    Util Function
    - Added a basic task waiting function from the samples

--- automation/vsphere_utils.py
+++ automation/vsphere_utils.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from pyVmomi import vim
+from pyVmomi import vmodl
 
+
+# From: various files in pyvmomi-community-samples
 def get_obj(content, vimtype, name):
     """
     Recursively finds and returns named object of specified type
@@ -22,6 +26,57 @@ def get_obj(content, vimtype, name):
 # TODO: function to apply a given operation to all objects in a view
 
 
+# From: tools/tasks.py in pyvmomi-community-samples
+def wait_for_tasks(service_instance, tasks):
+    """
+    Given the service instance si and tasks, it returns after all the tasks are complete
+    :param service_instance:
+    :param tasks:
+    :return:
+    """
+    property_collector = service_instance.content.propertyCollector
+    task_list = [str(task) for task in tasks]
+    # Create filter
+    obj_specs = [vmodl.query.PropertyCollector.ObjectSpec(obj=task)
+                 for task in tasks]
+    property_spec = vmodl.query.PropertyCollector.PropertySpec(type=vim.Task,
+                                                               pathSet=[],
+                                                               all=True)
+    filter_spec = vmodl.query.PropertyCollector.FilterSpec()
+    filter_spec.objectSet = obj_specs
+    filter_spec.propSet = [property_spec]
+    pcfilter = property_collector.CreateFilter(filter_spec, True)
+    try:
+        version, state = None, None
+        # Loop looking for updates till the state moves to a completed state.
+        while len(task_list):
+            update = property_collector.WaitForUpdates(version)
+            for filter_set in update.filterSet:
+                for obj_set in filter_set.objectSet:
+                    task = obj_set.obj
+                    for change in obj_set.changeSet:
+                        if change.name == 'info':
+                            state = change.val.state
+                        elif change.name == 'info.state':
+                            state = change.val
+                        else:
+                            continue
+
+                        if not str(task) in task_list:
+                            continue
+
+                        if state == vim.TaskInfo.State.success:
+                            # Remove task from taskList
+                            task_list.remove(str(task))
+                        elif state == vim.TaskInfo.State.error:
+                            raise task.info.error
+            # Move to next version
+            version = update.version
+    finally:
+        if pcfilter:
+            pcfilter.Destroy()
+
+
 # From: getallvms.py in pyvmomi-community-samples
 def print_vm_info(virtual_machine):
     """
@@ -38,10 +93,8 @@ def print_vm_info(virtual_machine):
     print("Bios UUID     : ", summary.config.uuid)
     print("State         : ", summary.runtime.powerState)
     if summary.guest:
-        ip_address = summary.guest.ipAddress
-        tools_version = summary.guest.toolsStatus
-        print("VMware-tools  : ", tools_version)
-        print("IP            : ", ip_address)
+        print("VMware-tools  : ", summary.guest.toolsStatus)
+        print("IP            : ", summary.guest.ipAddress)
     if summary.runtime.question:
         print("Question  : ", summary.runtime.question.text)
     if summary.config.annotation:
