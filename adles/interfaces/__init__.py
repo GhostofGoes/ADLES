5ca184b4068925b19d359b7b3edd38900565ff63
Christopher Goes <ghostofgoes@gmail.com>
2017-04-26 21:01:48 -0700

    Ok, THAT should fix it

--- adles/interfaces/__init__.py
+++ adles/interfaces/__init__.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
 from .interface import Interface
-from .vsphere_interface import VsphereInterface
-from .docker_interface import DockerInterface
 
 __all__ = ['interface', 'vsphere_interface', 'docker_interface']
